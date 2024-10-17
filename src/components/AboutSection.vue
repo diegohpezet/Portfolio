02ad2ed45c3c794c08cc9f655578c9b74504e891
diegohpezet <diegohpezet@gmail.com>
2024-10-17 10:09:34 -0300

    feat: add about section

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class="text-gray-900 dark:text-white text-lg">
+    <h2 class="text-4xl font-bold font-sans mb-3">Hi there!</h2>
+    <p>
+      I'm a passionate Fullstack Developer and Programming Educator with a deep love for coding and teaching. With
+      experience in both frontend and backend technologies, I build efficient, scalable web applications while also
+      sharing my knowledge with aspiring developers.
+    </p>
+    <br />
+    <blockquote class="px-4 border-s-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800">
+      <p>
+        As a <strong>developer</strong>, I specialize in creating robust APIs, user-friendly interfaces, and secure
+        systems using frameworks like Laravel and Vue. I constantly challenge myself to learn new tools and stay updated
+        with industry trends.
+      </p>
+      <br />
+      <p>
+        As a <strong>teacher</strong>, I help students grasp the fundamentals of programming, guiding them through
+        problem-solving, best practices, and project-based learning.
+      </p>
+    </blockquote>
+    <br />
+    <p>
+      I'm always eager to collaborate, innovate, and contribute to meaningful projects. Let's build something amazing
+      together!
+    </p>
+  </article>
+</template>
