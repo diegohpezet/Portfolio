95c3c7aeb8bde0bcc599ad80e5770fc9861430c9
diegohpezet <diegohpezet@gmail.com>
2024-12-04 10:05:00 -0300

    replace pictures

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -6,7 +6,7 @@ import ThemeSwitcher from './ThemeSwitcher.vue';
   <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3">
       <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+        <img src="../assets/images/avatar.jpg" class="h-8 rounded-full" alt="My avatar" />
       </a>
 
       <ThemeSwitcher />
