7aaa258ab706334dd04552c2e6447a785432d500
diegohpezet <diegohpezet@gmail.com>
2025-09-29 15:38:20 -0300

    update my personal information

--- src/components/profile/ProfilePicture.vue
+++ src/components/profile/ProfilePicture.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <img class="w-44 h-44 rounded-full border-2 border-gray-700 dark:border-gray-50" src="../../assets/images/me.jpg"
-      alt="Profile avatar">
+    <img class="w-44 h-44 rounded-full border-2 border-gray-700 dark:border-gray-50"
+      src="https://media.licdn.com/dms/image/v2/D4D03AQGSIsvivPzfYw/profile-displayphoto-shrink_200_200/B4DZZtX2PkGwAY-/0/1745591690653?e=1761782400&v=beta&t=u9DsbLfSzOi0CFc_kO1rC4rsghL3BeZqDpEH9ysZ0Ok"
+      alt="Profile avatar"
+    />
   </div>
 </template>
