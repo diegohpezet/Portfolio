4c6513752eb7cf626a720d8a57f8bc3f964d96d4
diegohpezet <diegohpezet@gmail.com>
2025-09-29 15:44:54 -0300

    update role and description

--- src/components/ProfileSection.vue
+++ src/components/ProfileSection.vue
@@ -9,10 +9,11 @@ import JobInfo from './profile/JobInfo.vue';
 
     <article class="col-span-2 mt-2 md:mt-0">
       <h1 class="text-4xl dark:text-white font-bold">Diego Pezet</h1>
-      <span class="text-gray-500 dark:text-gray-400">Fullstack Developer</span>
+      <span class="text-gray-500 dark:text-gray-400">Quality Engineer</span>
       <p class="dark:text-white italic">
-        I believe in the power of code to create, transform, and shape the future. Every project I undertake is an
-        opportunity to leave a positive impact
+        I believe in the power of technology to shape the future, inspire learning, and drive 
+        meaningful change. Every project I contribute to is a chance to elevate standards 
+        and empower others.
       </p>
       <JobInfo />
     </article>
