46e75179144944fc00d29a395b70c44ab176c84c
diegohpezet <diegohpezet@gmail.com>
2026-02-01 21:48:00 -0300

    add helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Container } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
+import { Helmet } from "react-helmet";
 import PersonalInfo from "./_components/PersonalInfo";
 import ContentTabs from "./_components/ContentTabs";
 import theme from "./theme";
@@ -7,6 +8,12 @@ import theme from "./theme";
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Helmet>
+        <title>Portfolio | diegohpezet</title>
+        <meta name="description" content="Welcome to my personal portfolio website." />
+        <link rel="canonical" href="https://diegohpezet.dev/" />
+        <link rel="icon" type="image/png" href="/favicon.ico" />
+      </Helmet>
       <Container maxWidth="md" sx={{ mt: 4 }}>
         <PersonalInfo />
 
